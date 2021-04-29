d21ef44474227c466b94ba4f10d70bb3b2d6095f
Scott Shelton <chris.scott.shelton@gmail.com>
2021-04-29 15:51:29 -0400

    Confirmed Connectivity

--- src/client/scenes/HelloWorldScene.ts
+++ src/client/scenes/HelloWorldScene.ts
@@ -1,39 +1,29 @@
 import Phaser from 'phaser'
+import * as Colyseus from 'colyseus.js'  //colyseus.js is correct for the client
 
 export default class HelloWorldScene extends Phaser.Scene
 {
+    private client!: Colyseus.Client
+
     constructor()
     {
         super('hello-world')
     }
 
-    preload()
+    init()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.client = new Colyseus.Client('ws://localhost:8001');
     }
 
-    create()
+    preload()
     {
-        this.add.image(400, 300, 'sky')
+    }
 
-        const particles = this.add.particles('red')
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+    async create()
+    {
+        const room: Colyseus.Room = await this.client.joinOrCreate('my_room')
 
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
+        console.log(`Room: ${room.name}, Id: ${room.id}`);
     }
 }
