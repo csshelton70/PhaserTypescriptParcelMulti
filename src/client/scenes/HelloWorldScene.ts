f7b9ebdc285dde1bc2c0b3a4d468fd3781741dae
Scott Shelton <chris.scott.shelton@gmail.com>
2021-04-29 19:15:09 -0400

    Started adding the Lobby

--- src/client/scenes/HelloWorldScene.ts
+++ src/client/scenes/HelloWorldScene.ts
@@ -4,6 +4,8 @@ import * as Colyseus from 'colyseus.js'  //colyseus.js is correct for the client
 export default class HelloWorldScene extends Phaser.Scene
 {
     private client!: Colyseus.Client
+    private lobby!: Colyseus.Room
+    private allRooms: any
 
     constructor()
     {
@@ -22,8 +24,62 @@ export default class HelloWorldScene extends Phaser.Scene
 
     async create()
     {
-        const room: Colyseus.Room = await this.client.joinOrCreate('my_room')
+        this.lobby = await this.client.joinOrCreate('lobby')
+        this.onjoin();
+        this.client.joinOrCreate("my_room");
+    }
 
-        console.log(`Room: ${room.name}, Id: ${room.id}`);
+    onjoin()
+    {
+        this.lobby.onMessage("rooms", (rooms) =>
+        {
+            this.allRooms = rooms;
+            this.update_full_list()
+
+            console.log("Received full list of rooms:", this.allRooms)
+        });
+
+        this.lobby.onMessage("+", ([ roomId, room ]) =>
+        {
+            const roomIndex = this.allRooms.findIndex((room) => room.roomId === roomId);
+            if (roomIndex !== -1)
+            {
+                console.log("Room update:", room);
+                this.allRooms[ roomIndex ] = room;
+
+            }
+            else
+            {
+                console.log("New room", room)
+                this.allRooms.push(room);
+            }
+            this.update_full_list();
+        });
+
+        this.lobby.onMessage("-", (roomId) =>
+        {
+            console.log("Room removed", roomId)
+            this.allRooms = this.allRooms.filter((room) => room.roomId !== roomId);
+            this.update_full_list();
+        });
+
+        this.lobby.onLeave(() =>
+        {
+            this.allRooms = [];
+            this.update_full_list();
+            console.log("Bye, bye!");
+        });
+    }
+
+    update_full_list()
+    {
+        console.log(`Full List:${this.allRooms}`);
+
+        /* var el = document.getElementById('all_rooms');
+         el.innerHTML = allRooms.map(function (room)
+         {
+             return "<li><code>" + JSON.stringify(room) + "</code></li>";
+         }).join("\n");
+ */
     }
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,10 @@ import express from 'express'
 import { Server } from 'colyseus'
 import { monitor } from '@colyseus/monitor'
 //import socialRoutes from '@colyseus/social/express'
+import { join } from 'path';
 
+
+import { LobbyRoom } from 'colyseus';
 import { MyRoom } from './rooms/MyRoom'
 
 const port = Number(process.env.port) || 8001
@@ -16,9 +19,15 @@ const gameServer = new Server({
     server: createServer(app)
 })
 
-gameServer.define('my_room', MyRoom)
+gameServer.define('lobby', LobbyRoom)
+gameServer.define('my_room', MyRoom).enableRealtimeListing()
 
 app.use('/colyseus', monitor())
+
+app.get('*', (req: any, res: any) =>
+{
+    res.sendFile(join(__dirname, '../client', 'index.html'));
+});
 
 gameServer.listen(port)
 
